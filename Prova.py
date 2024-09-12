de5be4ef9d4c72a55e8e496eedaa2003bbb02cdd
Jadison-ara <jadisonaragao1@gmail.com>
2024-09-11 23:35:52 -0300

    Delete Prova.py

--- Prova.py
+++
@@ -1,9 +0,0 @@
-altura = int (input("Digite o valor da altura do retângulo :"))
-
-base = int (input("Digite o valor da base do retângulo :"))
-
-area = altura * base
-
-print (f"Altura:{altura} x Base:{base}  = Área: {area}")
-
-
